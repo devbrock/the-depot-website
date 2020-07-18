7b52f56b29963fd75ec4235ad21b6560bb883846
devbrock <tbrockb@gmail.com>
2020-07-17 22:48:45 -0500

    update pictures since fb changed the cdn

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -8,49 +8,49 @@ import Carousel, { Modal, ModalGateway } from "react-images"
 const photos = [
   {
     src:
-      "https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/97168717_137097397906005_21079458970599424_n.jpg?_nc_cat=110&_nc_sid=0debeb&_nc_oc=AQm0qmaUwX6fNuNED5xN4bUedzXfIuBcW65_wa_V-H6UW-eAg_Rs7HF6QHcZwdgqy2A&_nc_ht=scontent-lax3-1.xx&oh=17370accd44e47f6faabe320da347845&oe=5EDBD182",
+      "https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/97168717_137097397906005_21079458970599424_n.jpg?_nc_cat=110&_nc_sid=0debeb&_nc_ohc=p1MqpF5k4P0AX9xqZT4&_nc_ht=scontent-dfw5-1.xx&oh=c2e8a67823ffbf153a69cb1e053b9cab&oe=5F36C902",
     width: 2,
     height: 3,
     alt: "",
   },
   {
     src:
-      "https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/96524574_137097444572667_1402201479481131008_n.jpg?_nc_cat=102&_nc_sid=0debeb&_nc_oc=AQnN0sSvnlfRTJZJcul8XJRNUopqHjA-7ETvVW3rRhdvylW6_WBoQRev2kobwA7SFh4&_nc_ht=scontent-lax3-1.xx&oh=4c52aa04a8f9162f61d190cf55b1b59e&oe=5EDA920C",
-    width: 2,
-    height: 2,
-    alt: "alt text test",
-  },
-  {
-    src:
-      "https://scontent-lax3-2.xx.fbcdn.net/v/t1.0-9/96806728_137097511239327_5166325145706430464_n.jpg?_nc_cat=111&_nc_sid=0debeb&_nc_oc=AQk1i4fIy66Gdm8XOQYmHQ0EUjWAOeZK_WIEEsz4jEYQoYBQ5yExpDavoco45EAILzw&_nc_ht=scontent-lax3-2.xx&oh=ff2abaf26f36aa46029f60af84a1826f&oe=5EDB29DC",
+      "https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-9/96524574_137097444572667_1402201479481131008_n.jpg?_nc_cat=102&_nc_sid=0debeb&_nc_ohc=tHn3MpzsdZAAX-uTgOq&_nc_ht=scontent-dfw5-2.xx&oh=3767b11d7bac40869d32a645c6c503e5&oe=5F397E0C",
     width: 2,
     height: 2,
     alt: "",
   },
   {
     src:
-      "https://scontent-lax3-2.xx.fbcdn.net/v/t1.0-9/96593672_137097567905988_719985906362089472_n.jpg?_nc_cat=106&_nc_sid=0debeb&_nc_oc=AQkZqFmTyLu237SqoZqu3f0RlmgXifVlRnYiRO3HKbR-fdKvuEnf9o_ONZQ3MlGCsuU&_nc_ht=scontent-lax3-2.xx&oh=45345e51ad5263bae6413e5bf036cdde&oe=5EDC8960",
+      "https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/96806728_137097511239327_5166325145706430464_n.jpg?_nc_cat=111&_nc_sid=0debeb&_nc_ohc=7ZzLumqpK0IAX8SJVcT&_nc_ht=scontent-dfw5-1.xx&oh=3d33d5b656d28f1538d5d36cb5b589a4&oe=5F36215C",
     width: 2,
     height: 2,
     alt: "",
   },
   {
     src:
-      "https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/97029759_137097451239333_6610533097559556096_n.jpg?_nc_cat=108&_nc_sid=0debeb&_nc_oc=AQmiDjmaNB8Ihm7NMGpk485wGNocncpoc5_Ssa760rihfWD3cffTFWKcmPIOmp5xnZM&_nc_ht=scontent-lax3-1.xx&oh=ad6bb47659e7ccdc447f67b8b7833c6d&oe=5EDBB4BF",
+      "https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-9/96593672_137097567905988_719985906362089472_n.jpg?_nc_cat=106&_nc_sid=0debeb&_nc_ohc=15jS7vNX-X4AX-EFNM4&_nc_ht=scontent-dfw5-2.xx&oh=7d63f34820732e10a0f31a217cf5a67c&oe=5F3780E0",
     width: 2,
     height: 2,
     alt: "",
   },
   {
     src:
-      "https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/96581897_137097501239328_6743356649110503424_n.jpg?_nc_cat=103&_nc_sid=0debeb&_nc_oc=AQmD2mdIoRZBfDRAWI8uwn-4b6_ng3Ve0nywhQaLYjrtiP9odHNGNYmhO4pGDbPxaEY&_nc_ht=scontent-lax3-1.xx&oh=327773e742dc1825129e351aae29f005&oe=5EDE2702",
+      "https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-9/97029759_137097451239333_6610533097559556096_n.jpg?_nc_cat=108&_nc_sid=0debeb&_nc_ohc=pM3IgxJCoPkAX-jJ8gN&_nc_ht=scontent-dfw5-2.xx&oh=2265a0d33f44b99eb150ebc2df87f489&oe=5F36AC3F",
     width: 2,
     height: 2,
     alt: "",
   },
   {
     src:
-      "https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/96821565_137097421239336_5789212054938189824_n.jpg?_nc_cat=104&_nc_sid=0debeb&_nc_oc=AQljYDkujyzhIXuw5xjLv-UJl9jZ1TlYz5L4v7q9aQK_oN-xX9ZZy1R-c24XDbBLU2E&_nc_ht=scontent-lax3-1.xx&oh=29f96ba9fdd31f4dabbfb4899941db88&oe=5EDB9776",
+      "https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/96581897_137097501239328_6743356649110503424_n.jpg?_nc_cat=103&_nc_sid=0debeb&_nc_ohc=MY3Korz-ab8AX_0_gqK&_nc_ht=scontent-dfw5-1.xx&oh=42537553eb56a3997df8a4147e1b4a90&oe=5F391E82",
+    width: 2,
+    height: 2,
+    alt: "",
+  },
+  {
+    src:
+      "https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-9/96821565_137097421239336_5789212054938189824_n.jpg?_nc_cat=104&_nc_sid=0debeb&_nc_ohc=Ulposez5aQAAX_O7RHZ&_nc_ht=scontent-dfw5-2.xx&oh=26f17ac9fa3291f7199ff05699395c9c&oe=5F368EF6",
     width: 2,
     height: 2,
     alt: "",
